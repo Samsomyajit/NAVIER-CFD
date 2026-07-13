77a849e36e346ed583656be17b547b1a0d04ae1d
Somyajit Chakraborty <somyajitchppr@gmail.com>
2026-07-13 17:14:57 +0800

    Expose canonical CFD dataset adapters and loaders

--- src/navier_cfd/datasets/__init__.py
+++ src/navier_cfd/datasets/__init__.py
@@ -1,3 +1,33 @@
+from .core import (
+    AdaptedDataset,
+    AdapterRegistry,
+    BUILTIN_DATASET_PROFILES,
+    CFDBatch,
+    CFDSample,
+    DatasetAdapter,
+    DatasetAdapterError,
+    DatasetProfile,
+    DatasetSubset,
+    collate_cfd_samples,
+    make_dataloaders,
+    split_dataset,
+    split_indices,
+)
 from .huggingface import HuggingFaceDatasetManager
 
-__all__ = ["HuggingFaceDatasetManager"]
+__all__ = [
+    "AdaptedDataset",
+    "AdapterRegistry",
+    "BUILTIN_DATASET_PROFILES",
+    "CFDBatch",
+    "CFDSample",
+    "DatasetAdapter",
+    "DatasetAdapterError",
+    "DatasetProfile",
+    "DatasetSubset",
+    "HuggingFaceDatasetManager",
+    "collate_cfd_samples",
+    "make_dataloaders",
+    "split_dataset",
+    "split_indices",
+]
